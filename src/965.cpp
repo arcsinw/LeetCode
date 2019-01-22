6720272c7da5f934f83586c2ce4444c22dccfb40
arcsinw <wangx86@live.com>
2019-01-22 21:45:58 +0800

    add 965

---
+++ src/965.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+bool isUnivalTree(TreeNode* root) {
+    int value = root->val;
+
+    queue<TreeNode*> que;
+
+    while(is_empty(que))
+    {
+
+    }
+}
+
+int main()
+{
+
+}
